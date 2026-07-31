c8bf0bb7fa1b4f949c8959627212ecd426f743bc
ddv-dev <demekhindaniil@icloud.com>
2026-07-31 12:15:06 +0000

    css

--- resources/views/concrete/hero.blade.php
+++ resources/views/concrete/hero.blade.php
@@ -1,14 +1,11 @@
 <section class="section-bottom py-20">
     <div class="d-flex column container gap-8">
         <h3 class="fw-medium fs-16"> — М100–М500 · ГОСТ 7473-2010 · Гравий и гранит · Тёплый зимой</h3>
-        <div class="fs-40 fw-bold line-h-120">Купить бетон с доставкой <br> по МО — от 5 600 ₽/м³, <br> ГОСТ 7473-2010,
-            <br>
-            паспорт качества <br>
-            в каждой
-            машине
+        <div class="fs-40 fw-bold line-h-120">Купить бетон с доставкой по МО — от 5 600 ₽/м³, ГОСТ 7473-2010, паспорт
+            качества в каждой машине
         </div>
         <p class="line-h-140">Производим и доставляем товарный бетон всех марок. <br>Своя лаборатория, паспорт с каждой
-            машиной, работа 24/7.
+            машиной, работа 24/7
         </p>
         <div class="row gap-20 mt-40">
             <a href="" class="btn btn-primary btn-arrow-right-white fs-18 fw-semibold br-20">Рассчитать
